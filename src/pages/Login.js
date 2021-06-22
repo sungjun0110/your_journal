29d2ca7e22a9c656a10c9ae12b422215fe2c8a59
sungjun0110 <55672234+sungjun0110@users.noreply.github.com>
2021-06-21 17:20:43 -0700

    initial commit

---
+++ src/pages/Login.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = (e) => {
+    
+  }
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Login
