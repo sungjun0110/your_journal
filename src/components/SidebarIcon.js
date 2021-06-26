061ac84ad6059f1622902c680af30f63e4b8ee6b
sungjun0110 <55672234+sungjun0110@users.noreply.github.com>
2021-06-26 00:01:14 -0700

    finished hamburger menu button

--- src/components/SidebarIcon.js
+++ src/components/SidebarIcon.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './SidebarIcon.css'
 
 function SidebarIcon() {
+  const [isClicked, setIsClicked] = useState(false);
+
   return (
-    <div id="hambergerMenu">
-      <div className="burgerBtn">
-        
-      </div>
+    <div 
+      id="hambergerMenu" 
+      className={isClicked? "clicked" : null} 
+      onClick={() => setIsClicked(!isClicked)}
+    >
+      <div className="burgerBtn"></div>
     </div>
   )
 }
